8d4575d4c15bf8189118fa0de3a5ae31866f70dd
李辰 <2095055676@qq.com>
2024-10-18 23:19:37 +0800

    feat: register

---
+++ monorepo-combat/.prettierrc.cjs
@@ -0,0 +1,19 @@
+moudle.exports = {
+  printWidth: 100, // 单行最多一百个字符
+  tabWidth: 2, // 两行缩进
+  useTabs: false, // 不使用缩进符
+  semi: true, // 行尾需要有分号
+  singleQuote: true, // 使用单引号
+  quoteProps: "as-needed", // 对象的key仅仅在必要时需要银行
+  jsxSingleQuote: false, // jsx使用双引号
+  trailingComma: "all", // 末尾需要逗号
+  bracketSpacing: true, // 大括号末尾需要空格
+  arrowParens: "always", // 箭头函数只有一个参数时也需要括号
+  rangeStart: 0, // 每个文件格式化的范围是文件的全部内容
+  rangeEnd: Infinity,
+  requirePrama: false,
+  insertPrama: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "CRLF",
+};
---
+++ monorepo-combat/fass/Chapter3/hello-world.ts
@@ -0,0 +1,4 @@
+function hello(name: string): string {
+  return 'nihao' + name;
+}
+console.log(hello('cc'));
